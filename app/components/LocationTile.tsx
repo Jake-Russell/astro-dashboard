ee85074f1d882e394c150e0bb1f75ea97509e8c6
Jake <jake@megster.net>
2026-04-09 17:46:33 +0100

    Implement loading spinner

--- app/components/LocationTile.tsx
+++ app/components/LocationTile.tsx
@@ -1,85 +1,106 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getLatLng, getLocationName } from "../utils/getLocationData";
 import { useAstronomy } from "./AstronomyContext";
 import Tile from "./Tile";
 
 export const LocationTile = () => {
-    const { setLatitude, setLongitude, weatherData } = useAstronomy();
-    const [error, setError] = useState<string | null>(null);
+    const { setLatitude, setLongitude, weatherLoading, setWeatherData } = useAstronomy();
+    const [error, setError] = useState<string>();
+    const [isLoading, setIsLoading] = useState(false);
 
-    const [location, setLocation] = useState("");
-    const [locationDisplayName, setLocationDisplayName] = useState("");
+    const [location, setLocation] = useState<string>();
+    const [locationDisplayName, setLocationDisplayName] = useState<string>();
+
+    useEffect(() => {
+        if (isLoading !== weatherLoading) setIsLoading(weatherLoading);
+    }, [weatherLoading]);
+
+    const resetSearch = () => {
+        setIsLoading(true);
+        setWeatherData(undefined);
+        setLocation(undefined);
+        setLocationDisplayName(undefined);
+        setError(undefined);
+    };
 
     const handleUseLocation = () => {
+        resetSearch();
         if (!navigator.geolocation) {
+            setIsLoading(false);
             setError("Geolocation is not supported by your browser.");
             return;
         }
         navigator.geolocation.getCurrentPosition(
             async (position) => {
-                setLatitude(position.coords.latitude.toFixed(5));
-                setLongitude(position.coords.longitude.toFixed(5));
+                const userLatitude = position.coords.latitude.toFixed(5);
+                const userLongitude = position.coords.longitude.toFixed(5);
 
-                const location = await getLocationName(
-                    position.coords.latitude.toString(),
-                    position.coords.longitude.toString(),
-                );
-                setLocation(location.name);
-                setLocationDisplayName(location.displayName);
+                setLatitude(userLatitude);
+                setLongitude(userLongitude);
 
-                setError(null);
+                const locationResponse = await getLocationName(userLatitude, userLongitude);
+
+                setLocation(locationResponse.name);
+                setLocationDisplayName(locationResponse.displayName);
             },
             () => {
                 // TODO: Add more specific error handling
+                setIsLoading(false);
                 setError("Unable to retrieve your location.");
             },
         );
     };
 
     const handleLocationSearch = async () => {
+        if (!location) return;
+
+        resetSearch();
+
         const locationData = await getLatLng(location);
-        console.log(`Location data for ${location}:`, locationData);
         setLatitude(locationData.lat);
         setLongitude(locationData.lon);
-
         setLocationDisplayName(locationData.displayName);
     };
 
     return (
-        <Tile title="Location">
-            <button
-                className="w-full px-3 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition-colors duration-200 mb-4 cursor-pointer"
-                onClick={handleUseLocation}
-            >
-                Use My Location
-            </button>
-            <div className="flex flex-col gap-2">
-                <p>Or alternatively, search for a location:</p>
-                <input
-                    type="text"
-                    placeholder="Location"
-                    value={location}
-                    onChange={(e) => setLocation(e.target.value)}
-                    className="w-full rounded-lg bg-gray-50 border border-gray-300 p-3 text-gray-800 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                />
+        <>
+            <Tile title="Location">
                 <button
                     className="w-full px-3 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition-colors duration-200 mb-4 cursor-pointer"
-                    onClick={handleLocationSearch}
+                    onClick={handleUseLocation}
                 >
-                    Search
+                    Use My Location
                 </button>
-                <div className="text-gray-500 text-xs mt-1 select-none">
-                    Showing results for:
-                    {weatherData ? (
-                        <p>{locationDisplayName}</p>
-                    ) : (
-                        <span className="italic"> (not set)</span>
-                    )}
+                <div className="flex flex-col gap-2">
+                    <p>Or alternatively, search for a location:</p>
+                    <input
+                        type="text"
+                        placeholder="Location"
+                        value={location}
+                        onChange={(e) => setLocation(e.target.value)}
+                        className="w-full rounded-lg bg-gray-50 border border-gray-300 p-3 text-gray-800 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                    />
+                    <button
+                        className="w-full px-3 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700 transition-colors duration-200 mb-4 cursor-pointer"
+                        onClick={handleLocationSearch}
+                    >
+                        Search
+                    </button>
+                    <div className="text-gray-500 text-xs mt-1 select-none">
+                        Showing results for:{" "}
+                        {locationDisplayName || <span className="italic"> (not set)</span>}
+                    </div>
+                    <div> </div>
+                    {error && <div className="text-red-500 text-sm mt-1">{error}</div>}
                 </div>
-                <div> </div>
-                {error && <div className="text-red-500 text-sm mt-1">{error}</div>}
-            </div>
-        </Tile>
+            </Tile>
+            {isLoading && (
+                <div className="flex flex-col items-center justify-center gap-3 text-gray-600 mt-8">
+                    <div className="h-16 w-16 border-4 border-blue-600 border-t-transparent rounded-full animate-spin" />
+                    <span className="text-sm font-medium">Loading...</span>
+                </div>
+            )}
+        </>
     );
 };

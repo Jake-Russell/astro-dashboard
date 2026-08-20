759d06a3d4fee8aead1eb94ede0f409dab9560cc
Jake <jake@megster.net>
2026-08-20 20:32:44 +0100

    Update moonUtils time comparison

--- app/utils/moonUtils.ts
+++ app/utils/moonUtils.ts
@@ -1,11 +1,11 @@
-import { isBefore } from "date-fns";
+import { fromUnixTime, isBefore } from "date-fns";
 
 export const getAdjustedMoonRiseAndSet = (
     moonriseEpoch: number,
     moonsetEpoch: number,
     tomorrowMoonsetEpoch: number,
 ): { moonrise: number; moonset: number } => {
-    if (isBefore(moonsetEpoch, moonriseEpoch)) {
+    if (isBefore(fromUnixTime(moonsetEpoch), fromUnixTime(moonriseEpoch))) {
         return { moonrise: moonriseEpoch, moonset: tomorrowMoonsetEpoch };
     }
 

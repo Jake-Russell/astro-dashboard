09e774d5a32f61c1a40b0a298199672972edc526
Jake <jake@megster.net>
2026-08-25 08:51:33 +0100

    Setup vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { ThemeProvider } from "contexts/ThemeContext";
 
@@ -99,6 +100,7 @@ export default function RootLayout({
                     }}
                 />
                 <ThemeProvider>{children}</ThemeProvider>
+                <Analytics />
             </body>
         </html>
     );
